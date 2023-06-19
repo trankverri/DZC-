afcfeb8e9d08d4da2f4a1737f8d2d410bb7d757e
trankverri <trankverri@vk.com>
2023-06-19 13:09:34 +0300

    add DZ-end1

---
+++ DZ-end1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, препод.!");
+
+// обявляем строковый массив
+string[] AR = {"Маша","Даша","Мария","Дарья","Катя"};
+
+string Metod1(string[])
+{
+   int index=0;
+   string[] Temperatur= {};
+   string Text ="";
+while (index<Metod1.Length)
+{
+    if (AR[index].Length<=0)
+    {
+        Text= AR[index];
+        Temperatur.add(Text);
+    }
+    Console.Write("{Temperatur}\t");
+}
+}
